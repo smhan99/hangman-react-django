8f89b563bf910e4c2ab62a4a1a0875635fc6e3f6
Shelley McHardy <shelleymcq@Shelleys-Mini.attlocal.net>
2023-04-26 08:23:59 -0400

    added login page

--- hangman-frontend/src/App.js
+++ hangman-frontend/src/App.js
@@ -1,16 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import './App.css';
+import "./App.css";
 import { Game } from "./Game";
-
-
+import { Login } from "./Login";
 //might be useful to have a <NavBar />
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
-          <Route path="/" element={ <Game /> } />
-          {/* <Route path="/hangman-react-django/login" element={ <LoginPage /> } /> */}
+          <Route path="/" element={<Game />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </Router>
     </div>
---
+++ hangman-frontend/src/Login.js
@@ -0,0 +1,57 @@
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+
+export const Login = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const data = new FormData(e.currentTarget);
+    console.log({
+      email: data.get("email"),
+      password: data.get("password"),
+    });
+  };
+  return (
+    <Container component="main" maxWidth="xs">
+      <Box
+        sx={{
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="username"
+            label="Username"
+            name="username"
+            autoFocus
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+          />
+
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Log In
+          </Button>
+        </Box>
+      </Box>
+    </Container>
+  );
+};

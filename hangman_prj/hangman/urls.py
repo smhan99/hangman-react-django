1ba4ed150c7e873430fb80afc5b5bd7c1d41edee
Abhijith Chandra Mergu <a.mergu@ufl.edu>
2023-04-27 02:38:08 -0400

    Reverting url removal

--- hangman_prj/hangman/urls.py
+++ hangman_prj/hangman/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('login_view', views.login_view, name='login_view'),
     path('logout_view', views.logout_view, name='logout_view'),
-
+    path('register', views.register, name='register'),
 ]

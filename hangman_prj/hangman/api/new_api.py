ea4f5aad1b60b99fd5fbe79b8211ef60dc35b658
Abhijith Chandra Mergu <a.mergu@ufl.edu>
2023-04-28 16:52:22 -0400

    Fixing the leaderboard API

--- hangman_prj/hangman/api/new_api.py
+++ hangman_prj/hangman/api/new_api.py
@@ -244,7 +244,7 @@ def get_leaderboard(_):
             }
         )
 
-    return Response({'leaderboard': result})
+    return Response({'response': {'leaderboard': result}})
 
 
 @api_view(['POST'])
